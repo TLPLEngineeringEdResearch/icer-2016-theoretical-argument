d8b204d0a0d649afd63e01b0b595f7cdb51bc36b
Brian Danielak <briandaniela.k+github@gmail.com>
2016-04-22 15:28:15 -0400

    Drive home points about structural and syntactic similarity
    Add example statements in javascript

---
+++ example-statements.js
@@ -0,0 +1,14 @@
+// Increment a counter
+x = x + 1
+
+// Advance a simulation forward
+t = t + dt
+
+// Move an object under uniform velocity
+x_position = x_position + displacement
+
+// Accelerate an object along the x-dimension
+x_position = x_position + displacement(t)
+
+// Jitter a plot point using a stochastic function
+y_position = y_position + random_noise()
